0a6b8f5f313f7b5dfbc7f55f8092b2fbea73a275
mrlolethan <ethanjoyce@rocketmail.com>
2015-02-01 20:23:36 -0330

    Add server join cooldown.

--- src/com/fuzzoland/BungeePortals/Commands/CommandBPortals.java
+++ src/com/fuzzoland/BungeePortals/Commands/CommandBPortals.java
@@ -12,9 +12,9 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import com.fuzzoland.BungeePortals.BungeePortals;
-
 import com.sk89q.worldedit.bukkit.selections.CuboidSelection;
 import com.sk89q.worldedit.bukkit.selections.Selection;
 
--- src/com/fuzzoland/BungeePortals/Listeners/EventListener.java
+++ src/com/fuzzoland/BungeePortals/Listeners/EventListener.java
@@ -3,14 +3,18 @@ package com.fuzzoland.BungeePortals.Listeners;
 import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.bukkit.ChatColor;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import com.fuzzoland.BungeePortals.BungeePortals;
 
@@ -18,6 +22,7 @@ public class EventListener implements Listener{
 
 	private BungeePortals plugin;
 	private Map<String, Boolean> statusData = new HashMap<String, Boolean>();
+	private final CooldownHandler cooldown = new CooldownHandler();
 	
 	public EventListener(BungeePortals plugin){
 		this.plugin = plugin;
@@ -27,6 +32,7 @@ public class EventListener implements Listener{
 	public void onPlayerMove(PlayerMoveEvent event) throws IOException{
 		Player player = event.getPlayer();
 		String playerName = player.getName();
+
 		if(!this.statusData.containsKey(playerName)){
 			this.statusData.put(playerName, false);
 		}
@@ -35,6 +41,12 @@ public class EventListener implements Listener{
 		if(plugin.portalData.containsKey(data)){
 			if(!this.statusData.get(playerName)){
 				this.statusData.put(playerName, true);
+				// Check if the player is on cooldown
+				if (cooldown.isOnCooldown(player)) {
+					player.sendMessage(ChatColor.RED + "Please wait " + CooldownHandler.COOLDOWN_SECONDS + " seconds until attempting to join again.");
+					return; // Player is on cooldown.
+				}
+
 				String destination = plugin.portalData.get(data);
 				if(player.hasPermission("BungeePortals.portal." + destination) || player.hasPermission("BungeePortals.portal.*")){
 					ByteArrayOutputStream baos = new ByteArrayOutputStream();
@@ -44,6 +56,9 @@ public class EventListener implements Listener{
 					player.sendPluginMessage(plugin, "BungeeCord", baos.toByteArray());
 					baos.close();
 					dos.close();
+
+					// Put the player on cooldown
+					cooldown.addToCooldown(player);
 				}else{
 					player.sendMessage(plugin.configFile.getString("NoPortalPermissionMessage").replace("{destination}", destination).replaceAll("(&([a-f0-9l-or]))", "\u00A7$2"));
 				}
@@ -52,6 +67,30 @@ public class EventListener implements Listener{
 			if(this.statusData.get(playerName)){
 				this.statusData.put(playerName, false);
 			}
+		}
+	}
+
+
+	private class CooldownHandler {
+		public static final int COOLDOWN_SECONDS = 10;
+		private List<Player> players = new ArrayList<Player>();
+
+		public synchronized void addToCooldown(final Player player) {
+			this.players.add(player);
+
+			new BukkitRunnable() {
+				public void run() {
+					removeFromCooldown(player);
+				}
+			}.runTaskLater(plugin, COOLDOWN_SECONDS * 20);
+		}
+
+		public boolean isOnCooldown(Player player) {
+			return this.players.contains(player);
+		}
+
+		public synchronized void removeFromCooldown(Player player) {
+			this.players.remove(player);
 		}
 	}
 }
